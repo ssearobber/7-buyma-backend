9194685fc68197813645a9e2cb9f88838f94ac4a
ssearobber <ssearobber@naver.com>
2021-05-09 11:04:38 +0900

    make it better

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": process.env.MYSQL_PASSWORD,
-    "database": "buyma_test",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USERNAME || mysqlUsername,
+    "password": process.env.MYSQL_PASSWORD || mysqlPassword,
+    "database": process.env.MYSQL_DATABASE || mysqlDatabase,
+    "host": process.env.MYSQL_HOST || mysqlHost,
     "dialect": "mysql"
   },
   "test": {
@@ -16,10 +16,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": process.env.MYSQL_PASSWORD,
-    "database": "buyma_test",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USERNAME || mysqlUsername,
+    "password": process.env.MYSQL_PASSWORD || mysqlPassword,
+    "database": process.env.MYSQL_DATABASE || mysqlDatabase,
+    "host": process.env.MYSQL_HOST || mysqlHost,
     "dialect": "mysql"
   }
 }
